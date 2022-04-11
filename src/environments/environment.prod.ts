eef7b9ab9144a6286a497cef8bee1c242461f470
Bellkim Keith Onggon <bellkimsonggon@su.edu.ph>
2022-04-11 12:50:58 +0800

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBFRHDnnAbLVoja4w9KEyeUpjvYGoDAbZ8",
+    authDomain: "newslist-50513.firebaseapp.com",
+    databaseURL: "https://newslist-50513-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "newslist-50513",
+    storageBucket: "newslist-50513.appspot.com",
+    messagingSenderId: "734551931092",
+    appId: "1:734551931092:web:2be703eb57028cddbbe341"
+  }
 };
